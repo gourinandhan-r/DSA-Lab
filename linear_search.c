10f94722ddf0a2ec4bd2ef694f2159c60debe6d0
Gourinandhan R <chn24bt025@ceconline.edu>
2025-10-04 14:56:51 +0530

    Add files via upload
    Added Linear Search in C

---
+++ linear_search.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+void main()
+{
+	int i, n, item, flag=0;
+	printf("Enter the size of the array: ");
+	scanf("%d", &n);
+	int arr[n];
+	printf("Enter the array elements: ");
+	for(i=0; i<n; i++)
+	{
+	scanf("%d", &arr[i]);
+	}
+	printf("The given array: ");
+	for(i=0; i<n; i++)
+		printf("%d \t", arr[i]);
+	printf("\n");
+	printf("Enter the item to search for:");
+	scanf("%d", &item);
+	for(i=0; i<n; i++)
+	{
+	if(arr[i] == item)
+	{
+	printf("Item found at index: %d\n", i);
+	flag = 1;
+        break; 
+		}
+	if(arr[i] != item)
+	{
+		flag=0;
+	}
+	}
+	if(!flag)
+		printf("Item not found");
+}
+
+
+    
