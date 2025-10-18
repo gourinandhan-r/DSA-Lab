f474e58f52d0caa6612bd126b66be560fd42a582
Gourinandhan R <chn24bt025@ceconline.edu>
2025-10-18 06:57:21 +0530

    Add files via upload

---
+++ InsertionSort.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+void display(int arr[], int n)
+	{
+		for(int i = 0; i < n; i++)
+			printf("%d\t", arr[i]);
+		printf("\n");
+	}
+void insertionSort(int arr[], int n)
+	{
+		int i, key, j;
+		for(i = 1; i <= n; i++)
+			{
+				key = arr[i];
+				j = i - 1;
+				while(j >= 0 && arr[j] > key)
+				{
+					arr[j+1] = arr[j];
+					j --;	
+				}
+			arr[j+1] = key;
+			}
+	}
+int main()
+	{
+	int n;
+	printf("Enter the no. of elements: ");
+	scanf("%d", &n);
+	int arr[n];
+	printf("Enter the array elements: ");
+	for(int i = 0; i < n; i++)
+			scanf("%d", &arr[i]);
+	printf("Unsorted Array: ");
+	display(arr, n);
+	insertionSort(arr, n);
+	printf("Sorted array: ");
+	display(arr, n);
+	return 0;
+	}
